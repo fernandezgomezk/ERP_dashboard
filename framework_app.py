bc6c364167d2f876c4c663c303b8065abe72b1e4
fernandezgomezk <kim.fernandezgomez@tno.nl>
2026-04-23 11:45:35 +0200

    leesbare labels

--- framework_app.py
+++ framework_app.py
@@ -8,6 +8,16 @@ import streamlit as st
 ##########################################
 #######Het definieren van de functies####
 #########################################
+# Dictionaries
+legend_labels = {
+    "d_energiequote": "Afname in procentpunt",
+    "d_%": "Afname in procentpunt"
+}
+
+title_labels = {
+    "d_energiequote": "Afname energiequote van huishoudens met energiearmoede",
+    "d_%": "Afname aandeel huishoudens met energiearmoede met een energiequote van meer dan acht procent"
+}
 
 def get_gemeentegrenzen(): #Deze functie laad de grenzen van de gemeentes in voor onze ruimtelijke kaart
  
@@ -42,11 +52,16 @@ def get_fig(gdf, output): #Deze functie maakt de daadwerkelijke kaart
         geojson = plot_gdf.geometry.__geo_interface__,
         locations = plot_gdf.index,
         color=plotted_column,
+        labels={plotted_column: legend_labels[plotted_column]},
         center={"lat": 52.15, "lon": 5.15}, #Zodat de kaart in Nederland begint en niet in de Atlantische Oceaan
         zoom=7.2 #Zodat je meteen overzicht hebt
     )
 
-    fig.update_layout(height=1200) #Zodat de kaart niet superklein is
+    fig.update_layout(
+        height=1200, #Zodat de kaart niet superklein is
+        title_text=title_labels[plotted_column],
+        title_x=0 # align title to the left 
+    ) 
 
     return fig
 
@@ -61,7 +76,8 @@ gdf = get_gemeentegrenzen() #De gemeentegrenzen inladen
 
 output = get_output() #De csv met data inladen
 
-plotted_column = st.selectbox("Indicator", ["d_energiequote", "d_%"]) #De gebruiker kan kiezen welke indicator hij/zij wil zien
+plotted_column = st.selectbox("Indicator", ["d_energiequote", "d_%"], # De gebruiker kan kiezen welke indicator hij/zij wil zien
+                              format_func=lambda x:title_labels[x]) # Omzetten naar titel tekst
 
 if st.button('render'): #Als je op render klikt, wordt de figuur geladen
     fig = get_fig(gdf, output)
